657301a023a2946c57887dfd445ce4e78847ff78
kazuma <fujishiro.k2577@gmail.com>
2025-07-23 16:58:12 +0900

    add App.vue main.js

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <baseLayout>
+    <template #mainBody>
+      <!--各種ビュー-->
+      <!--<homeView />-->
+      <router-view />
+    </template>
+  </baseLayout>
+</template>
+
+<script setup>
+  import baseLayout from './components/layout/baseLayout.vue';
+</script>
+
+<style>
+</style>
